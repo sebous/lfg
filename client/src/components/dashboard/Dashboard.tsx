15391f211da54f4a28d9426b332b04a8ce67dc4b
sebous <sebous@gmail.com>
2020-01-30 16:57:46 +0100

    up

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ import { PeopleInQueue } from "./PeopleInQueue";
 export const Dashbaord: React.FC = () => {
   return (
     <Box height="calc(100vh - 36px)">
-      <Box height="20%"><PeopleInQueue></PeopleInQueue></Box>
+      <Box height="20%"><PeopleInQueue /></Box>
       <Box height="60%"></Box>
       <Box height="20%"></Box>
     </Box>
