9e604186f11a1c9ec796beee86cbf12a11d5d499
sebous <sebous@gmail.com>
2020-05-11 17:44:19 +0200

    redo

--- client/src/components/routes/PrivateRoute.tsx
+++ client/src/components/routes/PrivateRoute.tsx
@@ -10,9 +10,8 @@ export interface PrivateRouteProps extends RouteProps {
 
 // TODO: refactor this via router-dom docs
 export const PrivateRoute: React.FC<PrivateRouteProps> = props => {
-  console.log(props);
-  const { restrictedPath } = props;
-  if (props.isAuthenticated) {
+  const { restrictedPath, isAuthenticated } = props;
+  if (isAuthenticated) {
     return <Route {...props} component={() => <Redirect to={{ pathname: restrictedPath }} />} />;
   }
   return <Redirect to={{ pathname: AUTHENTICATION_PATH }} />;
