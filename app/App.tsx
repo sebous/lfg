c5172f762df2ae069d73a39a35b73cd50d392a5f
sebous <sebous@gmail.com>
2020-11-25 18:54:28 +0100

    cleanup

--- app/App.tsx
+++ app/App.tsx
@@ -4,17 +4,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { apolloClient } from "./src/lib/apolloClient";
 import { Routes } from "./src/navigation/Routes";
-import { UserProvider } from "./src/providers/UserProvider";
 
 export default function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <UserProvider>
-        <NavigationContainer>
-          <StatusBar style="light" />
-          <Routes />
-        </NavigationContainer>
-      </UserProvider>
+      <NavigationContainer>
+        <StatusBar style="light" />
+        <Routes />
+      </NavigationContainer>
     </ApolloProvider>
   );
 }
