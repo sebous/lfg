85c89e8c03773563d93c0bec5c15943cf5286439
sebous <sebous@gmail.com>
2020-11-25 09:47:10 +0100

    logs

--- server/src/common/dbConnection.ts
+++ server/src/common/dbConnection.ts
@@ -1,7 +1,9 @@
 import { getConnectionOptions, createConnection } from "typeorm";
 
 export const createTypeormConn = async () => {
+  console.log("createTypeormConn");
   const connectionOpts = await getConnectionOptions(process.env.NODE_ENV);
+  console.log("connectionOpts", connectionOpts);
   return process.env.NODE_ENV === "production"
     ? createConnection({
         ...connectionOpts,
